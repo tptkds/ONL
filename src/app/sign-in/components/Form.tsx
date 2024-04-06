e20d9014bd86f7b83f5dfa7d704aaa4bee68b46a
KimYouGyoung <tptkds12@gmail.com>
2024-04-06 20:54:58 +0900

    저장..

--- src/app/sign-in/components/Form.tsx
+++ src/app/sign-in/components/Form.tsx
@@ -1,8 +1,9 @@
 const Form: React.FC = () => {
     return (
         <form className="flex flex-col">
-            <input type="email" />
-            <input type="password" />
+            <label>이메일 주소<input type="email" /></label>
+            <label>패스워드<input type="password" /></label>
+            
         </form>
     );
 };
