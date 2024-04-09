20c319b395204b2081304f1ee3ff4fe4817d9206
finn <tptkds12@gmail.com>
2024-04-10 02:58:01 +0900

    페이지네이션전까지 구현

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,22 +1,82 @@
-'use server';
+'use client';
 
-import getAllPostsServer from '@/service/post/getAllPostsServer';
+import { QueryClient, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase';
 import {
-    HydrationBoundary,
-    QueryClient,
-    dehydrate,
-} from '@tanstack/react-query';
+    collection,
+    getDocs,
+    limit,
+    orderBy,
+    query,
+    QueryDocumentSnapshot,
+    startAfter,
+    Timestamp,
+} from 'firebase/firestore';
+import { PAGE_SIZE } from '@/constants/post';
 
-export default async function Posts() {
-    const queryClient = new QueryClient();
-    // await queryClient.prefetchInfiniteQuery({
-    //     queryKey: ['posts', 'all'],
-    //    // queryFn: async ({ pageParam }) => getAllPostsServer(pageParam),
-    //     getNextPageParam: lastPage => lastPage.lastVisible,
-    //     initialPageParam: undefined,
-    //     pages: 1,
-    // });
+interface PostData {
+    authorId: string;
+    category: string;
+    content: string;
+    createdAt: Timestamp;
+    tags: string[];
+    title: string;
+    updatedAt: Timestamp;
+}
+
+async function fetchAllPosts(
+    page: number,
+    lastDocument?: QueryDocumentSnapshot<PostData>
+): Promise<PostData[]> {
+    const postsRef = collection(db, 'posts');
+    let dataQuery;
+    if (page === 0) {
+        dataQuery = query(postsRef, orderBy('createdAt'), limit(PAGE_SIZE));
+    } else {
+        if (!lastDocument)
+            throw new Error(
+                'Last document is required for pagination after the first page.'
+            );
+        dataQuery = query(
+            postsRef,
+            orderBy('createdAt'),
+            startAfter(lastDocument),
+            limit(PAGE_SIZE)
+        );
+    }
+    const querySnapshot = await getDocs(dataQuery);
+    return querySnapshot.docs.map(doc => doc.data() as PostData);
+}
+
+export default function Posts() {
+    const [page, setPage] = useState<number>(0);
+    const queryClient = useQueryClient();
+    const { status, data, error, isFetching } = useQuery<PostData[], Error>({
+        queryKey: ['posts', 'all', page],
+        queryFn: () => fetchAllPosts(page),
+    });
+
+    useEffect(() => {
+        if (status === 'success' && data && data.length > 0) {
+            queryClient.setQueryData(['posts', 'all', page], data);
+        }
+    }, [status, data, page, queryClient]);
+
     return (
-        <HydrationBoundary state={dehydrate(queryClient)}></HydrationBoundary>
+        <div>
+            {status === 'pending' && <div>Loading...</div>}
+            {status === 'error' && error && <div>Error: {error.message}</div>}
+            {status === 'success' && data && (
+                <ul>
+                    {data.map((post, index) => (
+                        <li key={index}>
+                            <h3>{post.title}</h3>
+                            <p>{post.content.substring(0, 100)}...</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
     );
 }
