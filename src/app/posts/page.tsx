c6c38a7c060dc0a8ef7382827a4f5d553988f9bc
finn <tptkds12@gmail.com>
2024-04-10 04:48:49 +0900

    페이지네이션 기능 구현 완료

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,10 +1,10 @@
 'use client';
-
-import { QueryClient, useQuery, useQueryClient } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { db } from '../firebase';
 import {
     collection,
+    DocumentData,
     getDocs,
     limit,
     orderBy,
@@ -27,55 +27,91 @@ interface PostData {
 
 async function fetchAllPosts(
     page: number,
-    lastDocument?: QueryDocumentSnapshot<PostData>
-): Promise<PostData[]> {
+    lastDocument?: QueryDocumentSnapshot<DocumentData>
+): Promise<{
+    data: PostData[];
+    lastDoc: QueryDocumentSnapshot<DocumentData> | undefined;
+    hasMore: boolean;
+}> {
     const postsRef = collection(db, 'posts');
     let dataQuery;
-    if (page === 0) {
-        dataQuery = query(postsRef, orderBy('createdAt'), limit(PAGE_SIZE));
+
+    if (page === 0 || !lastDocument) {
+        dataQuery = query(postsRef, orderBy('createdAt'), limit(PAGE_SIZE + 1));
     } else {
-        if (!lastDocument)
-            throw new Error(
-                'Last document is required for pagination after the first page.'
-            );
         dataQuery = query(
             postsRef,
             orderBy('createdAt'),
             startAfter(lastDocument),
-            limit(PAGE_SIZE)
+            limit(PAGE_SIZE + 1)
         );
     }
     const querySnapshot = await getDocs(dataQuery);
-    return querySnapshot.docs.map(doc => doc.data() as PostData);
+
+    const hasMore = querySnapshot.docs.length > PAGE_SIZE;
+
+    const data = querySnapshot.docs
+        .slice(0, PAGE_SIZE)
+        .map(doc => doc.data() as PostData);
+
+    const lastVisible = hasMore ? querySnapshot.docs[PAGE_SIZE - 1] : undefined;
+
+    return {
+        data,
+        lastDoc: lastVisible,
+        hasMore,
+    };
 }
 
 export default function Posts() {
-    const [page, setPage] = useState<number>(0);
-    const queryClient = useQueryClient();
-    const { status, data, error, isFetching } = useQuery<PostData[], Error>({
-        queryKey: ['posts', 'all', page],
-        queryFn: () => fetchAllPosts(page),
-    });
+    const [page, setPage] = useState(0);
+    const [lastDoc, setLastDoc] = useState<
+        QueryDocumentSnapshot<DocumentData> | undefined
+    >(undefined);
 
-    useEffect(() => {
-        if (status === 'success' && data && data.length > 0) {
-            queryClient.setQueryData(['posts', 'all', page], data);
-        }
-    }, [status, data, page, queryClient]);
+    const { isPending, isError, error, data, isFetching, isPlaceholderData } =
+        useQuery({
+            queryKey: ['posts', 'all', page],
+            queryFn: () => fetchAllPosts(page, lastDoc),
+            placeholderData: keepPreviousData,
+            staleTime: 5000,
+        });
 
+    const handleNextPage = () => {
+        setPage(old => old + 1);
+    };
+
+    const handlePrevPage = () => {
+        setPage(old => Math.max(old - 1, 0));
+    };
+    console.log(data?.hasMore || isPlaceholderData);
     return (
         <div>
-            {status === 'pending' && <div>Loading...</div>}
-            {status === 'error' && error && <div>Error: {error.message}</div>}
-            {status === 'success' && data && (
-                <ul>
-                    {data.map((post, index) => (
-                        <li key={index}>
-                            <h3>{post.title}</h3>
-                            <p>{post.content.substring(0, 100)}...</p>
-                        </li>
-                    ))}
-                </ul>
+            {isPending ? (
+                <div>Loading...</div>
+            ) : isError ? (
+                <div>Error: {error.message}</div>
+            ) : (
+                <>
+                    <ul>
+                        {data?.data.map((post, index) => (
+                            <li key={index}>
+                                <h3>{post.title}</h3>
+                                <p>{post.content.substring(0, 100)}...</p>
+                            </li>
+                        ))}
+                    </ul>
+                    <button onClick={handlePrevPage} disabled={page === 0}>
+                        Previous Page
+                    </button>
+                    <button
+                        onClick={handleNextPage}
+                        disabled={!(data?.hasMore || isPlaceholderData)}
+                    >
+                        Next Page
+                    </button>
+                    {isFetching ? <span> Loading...</span> : null}
+                </>
             )}
         </div>
     );
