52db5f96012b814d03b2000116627d369387f565
Finn <tptkds12@gmail.com>
2024-04-16 18:57:07 +0900

    북마크, 시청여부, 추천 포스트 기능 추가 중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,4 @@ import { useSession } from 'next-auth/react';
 
 export default function Home() {
     const { data } = useSession();
-    console.log(data?.user);
 }
--- src/service/account/getLikedPosts.ts
+++ src/service/account/getLikedPosts.ts
@@ -5,6 +5,5 @@ export default async function getLikedPosts(userId: string) {
     const userRef = doc(db, 'users', userId);
     const likedPostsRef = collection(userRef, 'likedPosts');
     const docsSnapshot = await getDocs(likedPostsRef);
-    console.log(docsSnapshot);
     return docsSnapshot.docs.map(doc => doc.id) || [];
 }
