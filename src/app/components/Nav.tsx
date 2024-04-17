9a432f1b18449e0b89f4f29fc012ff4dad5c8ec5
Finn <tptkds12@gmail.com>
2024-04-17 11:06:14 +0900

    검색창 스타일링

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -69,31 +69,31 @@ export default function Nav() {
                 <>
                     <div className="border-t border-gray-200 w-full"></div>
                     <ul className="flex justify-center px-16 bg-white text-base font-medium text-neutral-800">
-                        <li className="px-4 py-6 mx-6 li-hover-line relative cursor">
+                        <li className="mx-6 li-hover-line relative cursor">
                             <Link
                                 href="/film-of-the-year"
-                                className="flex items-center justify-center"
+                                className="h-full px-4 py-6 flex items-center justify-center"
                             >
                                 <BsAwardFill className="text-base mr-1" />
                                 <span>올해의영화</span>
                             </Link>
                         </li>
-                        <li className="px-4 py-6 mx-6  li-hover-line relative cursor">
+                        <li className="mx-6  li-hover-line relative cursor">
                             <Link
                                 href="/film-finder"
-                                className="flex items-center justify-center"
+                                className="h-full px-4 py-6 flex items-center justify-center"
                             >
                                 <MdLocalMovies className="text-base mr-1" />
                                 <span>영화탐색</span>
                             </Link>
                         </li>
-                        <li className="px-4 py-6 mx-6  li-hover-line relative cursor">
+                        <li className=" mx-6  li-hover-line relative cursor">
                             <Link
                                 href="/board/1"
-                                className="flex items-center justify-center"
+                                className="h-full px-4 py-6 flex items-center justify-center "
                             >
                                 <IoChatboxEllipses className="text-base mr-1" />
-                                <span>이야기공간</span>
+                                <span className="">이야기공간</span>
                             </Link>
                         </li>
                     </ul>
--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -3,7 +3,9 @@ import getPostsByKeyword from '@/service/post/getPostsByKeyword';
 import { Movie } from '@/types/movie';
 import { PostData } from '@/types/post';
 import { useQuery } from '@tanstack/react-query';
+import Link from 'next/link';
 import { useState, useEffect } from 'react';
+import { IoIosSearch } from 'react-icons/io';
 
 export default function SearchBar() {
     const [searchedText, setSearchedText] = useState<string>('');
@@ -40,7 +42,8 @@ export default function SearchBar() {
     };
 
     return (
-        <div>
+        <div className="relative w-full">
+            <IoIosSearch className="absolute top-1/2 right-2 translate-y-[-50%] text-gray-500 text-xl" />
             <input
                 type="text"
                 className="h-12 w-full border focus:outline-none p-3"
@@ -50,24 +53,54 @@ export default function SearchBar() {
             />
             {(searchedMovies && searchedMovies.length > 0) ||
             (searchedPosts && searchedPosts.length > 0) ? (
-                <div>
-                    {searchedMovies && searchedMovies.length > 0 && (
+                <div className="absolute top-12 z-10  w-full  max-w-full p-4 bg-white shadow-md">
+                    {searchedPosts && searchedPosts.length > 0 && (
                         <div>
-                            <h2>Movies</h2>
-                            <ul>
-                                {searchedMovies.map((movie: Movie) => (
-                                    <li key={movie.id}>{movie.title}</li>
+                            <h2 className="py-0.5 text-sm font-bold">
+                                검색된 게시글
+                            </h2>
+                            <ul className="overflow-hidden">
+                                {searchedPosts.map((post: PostData) => (
+                                    <li
+                                        key={post.postId}
+                                        className="py-0.5  text-sm "
+                                    >
+                                        <Link
+                                            href={`/post/${post.postId}`}
+                                            className="w-full flex items-center"
+                                        >
+                                            <span className="block max-w-full truncate">
+                                                {post.title}
+                                            </span>
+
+                                            <span className="text-xs"></span>
+                                        </Link>
+                                    </li>
                                 ))}
                             </ul>
                         </div>
                     )}
-                    {searchedPosts && searchedPosts.length > 0 && (
-                        <div>
-                            <h2>Posts</h2>
-                            <ul>
-                                {searchedPosts.map((post: PostData) => (
-                                    <li key={post.postId}>
-                                        {post.title} - {post.authorName}
+                    {searchedMovies && searchedMovies.length > 0 && (
+                        <div className="pt-3">
+                            <h2 className="py-0.5 text-sm font-bold">
+                                검색된 영화
+                            </h2>
+                            <ul className="overflow-hidden">
+                                {searchedMovies.map((movie: Movie) => (
+                                    <li
+                                        key={movie.id}
+                                        className="py-0.5  text-sm "
+                                    >
+                                        <Link
+                                            href={`/film-info/${movie.id}`}
+                                            className="w-full flex items-center"
+                                        >
+                                            <span className="block max-w-[90%] truncate">
+                                                {movie.title}
+                                            </span>
+
+                                            <span className="text-xs">{` (${movie.release_date.slice(0, 4)})`}</span>
+                                        </Link>
                                     </li>
                                 ))}
                             </ul>
