782a6a264451e22814f326efcba049e1deff9a2d
KimYouGyoung <tptkds12@gmail.com>
2024-04-28 21:23:46 +0900

    🚨 linter 오류 제거

--- src/app/my-page/watched/components/Movie.tsx
+++ src/app/my-page/watched/components/Movie.tsx
@@ -42,7 +42,6 @@ export default function Movie({
                     movieId={WatchedMovieData.movieId}
                     uId={uid}
                     watchedMovies={watchedMovies}
-                    rating={5}
                 />
             </div>
         </div>
